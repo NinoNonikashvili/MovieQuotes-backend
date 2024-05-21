54876ea31cd570e5ff1ad232454b345cc58becf5
Nino Nonikashvili <nonikashvilinino8799@gmail.com>
2024-05-21 13:45:38 +0400

    fix: use route names in tests

--- tests/Feature/AuthTest.php
+++ tests/Feature/AuthTest.php
@@ -16,14 +16,14 @@ describe('login', function () {
 	it('is not  accessible for auth user', function () {
 		//code here
 		$user = User::find(6);
-		$this->actingAs($user)->post('/api/login', [
+		$this->actingAs($user)->post(route('login'), [
 			'email'      => 'nino@gmail.com',
 			'password'   => '33333333',
 			'rememberMe' => false,
 		])->assertStatus(302);
 	});
 	it('returns error when email and password are omitted', function () {
-		$response = $this->post('/api/login', [
+		$response = $this->post(route('login'), [
 			'email'    => '',
 			'password' => '',
 		]);
@@ -32,21 +32,21 @@ describe('login', function () {
 	});
 
 	it('returns wrong password when password is short', function () {
-		$response = $this->post('/api/login', [
+		$response = $this->post(route('login'), [
 			'passwors' => '',
 		]);
 		//middleware EnsureExists responds with 404
 		$response->assertStatus(302);
 	});
 	it('returns user doesnt exist if so', function () {
-		$response = $this->post('/api/login', [
+		$response = $this->post(route('login'), [
 			'email' => 'bla@gmail.com',
 		]);
 		//middleware EnsureExists responds with 404
 		$response->assertStatus(404);
 	});
 	it('returns email not verified error if so', function () {
-		$response = $this->post('/api/login', [
+		$response = $this->post(route('login'), [
 			'email'      => 'nino@gmail.com',
 			'password'   => '33333333',
 			'rememberMe' => 'false',
@@ -54,7 +54,7 @@ describe('login', function () {
 		$response->assertStatus(200);
 	});
 	it('returns success if everything is ok', function () {
-		$response = $this->post('/api/login', [
+		$response = $this->post(route('login'), [
 			'email'      => 'naina@gmail.com',
 			'password'   => '11111111',
 			'rememberMe' => 'false',
@@ -63,74 +63,68 @@ describe('login', function () {
 	});
 });
 
-describe('logout', function(){
-	it('logs auth user out', function(){
+describe('logout', function () {
+	it('logs auth user out', function () {
 		$user = User::find(2);
-		$this->actingAs($user)->get('/api/logout')->assertStatus(204);
-
+		$this->actingAs($user)->get(route('logout'), )->assertStatus(204);
 	});
-	it('returns error when unauth user tries logout', function(){
-		$this->get('/api/logout')->assertStatus(302);
-	});
-});
-
-describe('google auth', function(){
-
-	it('creates user if does not exist and loggs in', function(){
-		 // Create a mock for the Socialite driver
-		 $abstractUser = Mockery::mock('Laravel\Socialite\Two\User');
-		 $abstractUser->shouldReceive('getId')->andReturn('1234567890');
-		 $abstractUser->shouldReceive('getEmail')->andReturn('sandro@example.com');
-		 $abstractUser->shouldReceive('getName')->andReturn('Test User');
- 
-		 $provider = Mockery::mock('Laravel\Socialite\Contracts\Provider');
-		 $provider->shouldReceive('user')->andReturn($abstractUser);
- 
-		 Socialite::shouldReceive('driver')->with('google')->andReturn($provider);
-
-		 // Perform the callback request
-		 $response = $this->get('/api/auth/callback');
-
-		 // Assert the user was authenticated
-		 $this->assertAuthenticated();
- 
-		 // Assert the user data in the database
-		 $this->assertDatabaseHas('users', [
-			 'email' => 'sandro@example.com',
-		 ]);
- 
-		 $response->assertStatus(200);
-
-
+	it('returns error when unauth user tries logout', function () {
+		$this->get(route('logout'))->assertStatus(302);
 	});
 });
 
-describe('reset password', function(){
-	it('sends email to the user', function(){
-		$this->post('/api/forgot-password', [
-			'email' => 'nino@gmail.com'
+describe('google auth', function () {
+	it('creates user if does not exist and loggs in', function () {
+		// Create a mock for the Socialite driver
+		$abstractUser = Mockery::mock('Laravel\Socialite\Two\User');
+		$abstractUser->shouldReceive('getId')->andReturn('1234567890');
+		$abstractUser->shouldReceive('getEmail')->andReturn('sandro@example.com');
+		$abstractUser->shouldReceive('getName')->andReturn('Test User');
+
+		$provider = Mockery::mock('Laravel\Socialite\Contracts\Provider');
+		$provider->shouldReceive('user')->andReturn($abstractUser);
+
+		Socialite::shouldReceive('driver')->with('google')->andReturn($provider);
+
+		// Perform the callback request
+		$response = $this->get(route('auth.callback'));
+
+		// Assert the user was authenticated
+		$this->assertAuthenticated();
+
+		// Assert the user data in the database
+		$this->assertDatabaseHas('users', [
+			'email' => 'sandro@example.com',
+		]);
+
+		$response->assertStatus(200);
+	});
+});
+
+describe('reset password', function () {
+	it('sends email to the user', function () {
+		$this->post(route('password.email'), [
+			'email' => 'nino@gmail.com',
 		])->assertStatus(200);
-
 	});
-	it('returns error if user does not exist with the email provided', function(){
-		$this->post('/api/forgot-password', [
-			'email' => 'niscsrfno@gmail.com'
+	it('returns error if user does not exist with the email provided', function () {
+		$this->post(route('password.email'), [
+			'email' => 'niscsrfno@gmail.com',
 		])->assertStatus(404);
 	});
-	it('updates password when correct token and data are provided', function(){
-		
+	it('updates password when correct token and data are provided', function () {
 		Notification::fake();
 		$url = '';
-		$this->post('/api/forgot-password', ['email' => 'leqso@gmail.com']);
+		$this->post(route('password.email'), ['email' => 'leqso@gmail.com']);
 		Notification::assertSentTo(User::where('email', 'leqso@gmail.com')->first(), ResetPassword::class, function (ResetPassword $notification) use (&$url) {
 			$url = $notification->toMail(User::where('email', 'leqso@gmail.com')->first())->actionUrl;
 			return true;
 		});
-		$a = explode('?', $url );
+		$a = explode('?', $url);
 		$b = explode('&', $a[1]);
 		$c = explode('=', $b[0]);
-		
-		$this->post('/api/reset-password', [
+
+		$this->post(route('password.update'), [
 			'email'            => 'leqso@gmail.com',
 			'password'         => '99999999',
 			'confirm_password' => '99999999',
@@ -141,5 +135,3 @@ describe('reset password', function(){
 		$this->assertTrue(Hash::check('99999999', $newPass));
 	});
 });
-
-
