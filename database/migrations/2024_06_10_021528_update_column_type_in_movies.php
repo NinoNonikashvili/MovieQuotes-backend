f5ca7b19b7cacef2af1a187c3d731b342a1c8f50
Nino Nonikashvili <nonikashvilinino8799@gmail.com>
2024-06-10 06:20:00 +0400

    fix:add migration to update column types for movies table

---
+++ database/migrations/2024_06_10_021528_update_column_type_in_movies.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('movies', function (Blueprint $table) {
+            $table->json('title')->change();
+            $table->json('description')->change();
+            $table->json('director')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('movies', function (Blueprint $table) {
+            $table->dropColumn(['title', 'description', 'director']);
+        });
+    }
+};
